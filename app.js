7a5c1546a4773ec48c41a3191e2b5c75f34d2510
Shawn-Cao <xiangke.cao@gmail.com>
2016-01-31 19:41:12 -0500

    packageing update

---
+++ app.js
@@ -0,0 +1,8 @@
+'use strict';
+angular.module('buzAnt', ['ui.bootstrap'])
+	.controller('AppController', function('$scope') {
+		loginPopup = function() {
+			$modal
+		}
+		$scope.register = loginPopup;
+	});
